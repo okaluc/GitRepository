068824a7381c5a9cc7bf56062ab3fff41c7278f9
nausiluke <okajeanluc@gmail.com>
2015-02-01 13:46:12 +0000

    Ajout de fichier java et html au projet

---
+++ BonjourUtilisateur.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package bonjourutilisateur;
+import java.util.Scanner;
+import java.util.*;
+
+
+/**
+ *
+ * @author nausiluke
+ */
+public class BonjourUtilisateur {
+
+    /**
+     * @param args the command line arguments
+     */
+    String nom="";
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner sc =new Scanner(System.in);
+        System.out.println("entrez votre nom: ");
+        String str = sc.nextLine();
+        System.out.println("bonjour "+str);
+        
+      
+        
+    }
+    
+}
---
+++ README.txt
@@ -0,0 +1,2 @@
+ce projet consiste a demander le nom de l'utilisateur
+ensuite lui afficher "bonjour nomUtilisateur"
